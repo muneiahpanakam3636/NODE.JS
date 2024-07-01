752490da764e33c9985d55e9daef485a0d74a2da
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-01 15:28:04 +0530

    Node

---
+++ OSmodule/two.js
@@ -0,0 +1,11 @@
+import os from "os"
+
+console.log(os.arch());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.release());
+console.log(os.hostname());
+console.log(os.type());
+
+
+console.log(os.tmpdir());
